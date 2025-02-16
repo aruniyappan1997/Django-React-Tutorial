a114b0bf7dad8be1f943aa2908cacd06b2834fe6
aruniyappan1997 <aruniyappan1997@gmail.com>
2025-02-16 22:54:06 +0530

    Added DB config and added the serializer

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        field = ["id", "username", "password"]
+        fields = ["id", "username", "password"]
         extra_kwargs = {"password":{"write_only":True}}
 
     def create(self, validated_data):
